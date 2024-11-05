1f97f6b8c56603b430653ce403d92aaaf8e7be54
joseMax1 <109931606+joseMax1@users.noreply.github.com>
2024-11-05 18:09:36 -0500

    Update Program.cs

--- etapa4/Program.cs
+++ etapa4/Program.cs
@@ -3,7 +3,9 @@
 // Grupo: 30
 
 // Enunciado del ejercicio:
-// Este programa solicita al usuario ingresar 10 números diferentes, y posteriormente los ordena según el método de ordenamiento que elija entre las siguientes opciones: Burbuja, Shell, Selección o Inserción. El sistema maneja excepciones para evitar duplicados y entradas no válidas. Además, permite almacenar los resultados ordenados en un archivo de texto llamado SortedNumbers.txt.
+// Este programa solicita al usuario ingresar 10 números diferentes, y posteriormente los ordena según el método de 
+//ordenamiento que elija entre las siguientes opciones: Burbuja, Shell, Selección o Inserción. El sistema maneja excepciones para evitar duplicados y 
+//entradas no válidas. Además, permite almacenar los resultados ordenados en un archivo de texto llamado SortedNumbers.txt.
 
 // El menú y las instrucciones que se muestran al usuario estarán en inglés. A lo largo del proceso, el programa:
 
